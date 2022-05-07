689c1087ecc21fc71e7b87a69eab704359f5257c
Yury <kotovyuriy27@gmail.com>
2022-05-08 00:36:18 +0300

    added login screen

--- ui/README.md
+++ ui/README.md
@@ -1,3 +1,9 @@
 TODO:
 - понять как компилить gtk (желательно gtk4)
-- 
+
+# Run command
+- g++ `pkg-config --cflags gtk+-3.0` -o main main.c `pkg-config --libs gtk+-3.0`
+
+# GTK 3 doc
+- https://docs.gtk.org/gtk3/
+- https://docs.gtk.org/gtk3/getting_started.html#building-applications
--- ui/main.c
+++ ui/main.c
@@ -1,38 +1,67 @@
+#include <iostream>
+#include <stdio.h>
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
+GtkWidget *window;
+GtkWidget* nick_name_box;
+GtkWidget* password_box;
+GtkWidget* submit_button;
+GtkWidget* login_box;
+
+void submit(GtkWidget* widget, GdkEvent  *event, gpointer data)
 {
-  g_print ("Hello World\n");
+  const char* nick = gtk_entry_get_text(GTK_ENTRY(nick_name_box));
+  const char* password = gtk_entry_get_text(GTK_ENTRY(password_box));
+
+  if (strlen(nick) == 0)
+  {
+    std::cout << "enter nick\n" << std::endl;
+    return;
+  }
+
+  // message must not be empty
+  if (strlen(password) == 0)
+  {
+      std::cout << "enter password" << std::endl;
+      return;
+  }
+
+  std::cout << "nick " << nick << std::endl;
+  std::cout << "password " << password << std::endl;
+
+  gtk_container_remove(GTK_CONTAINER(window), login_box);
 }
 
-static void
-activate (GtkApplication *app,
-          gpointer        user_data)
+static void activate (GtkApplication *app, gpointer user_data)
 {
-  GtkWidget *window;
-  GtkWidget *button;
-  GtkWidget *button_box;
-
   window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_window_set_title (GTK_WINDOW (window), "Chat");
+  gtk_window_set_default_size (GTK_WINDOW (window), 600, 800);
 
-  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  gtk_container_add (GTK_CONTAINER (window), button_box);
+  login_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+  // gtk_box_set_center_widget(GTK_CONTAINER(window), login_box);
 
-  button = gtk_button_new_with_label ("Hello World");
-  g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
-  g_signal_connect_swapped (button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
-  gtk_container_add (GTK_CONTAINER (button_box), button);
+  nick_name_box = gtk_entry_new();
+  gtk_widget_set_size_request(nick_name_box, 200, -1);
+  gtk_entry_set_placeholder_text(GTK_ENTRY(nick_name_box), "nickname");
+  gtk_box_pack_start(GTK_BOX(login_box), nick_name_box, FALSE, TRUE, 0);
 
+  password_box = gtk_entry_new();
+  gtk_widget_set_size_request(password_box, 200, -1);
+  gtk_entry_set_placeholder_text(GTK_ENTRY(password_box), "password");
+  gtk_box_pack_start(GTK_BOX(login_box), password_box, FALSE, TRUE, 0);
+
+  submit_button = gtk_button_new_with_label("login");
+	gtk_box_pack_start(GTK_BOX(login_box), submit_button, FALSE, TRUE, 0);
+  g_signal_connect(submit_button, "clicked", G_CALLBACK(submit), NULL);
+
+  gtk_container_add(GTK_CONTAINER(window), login_box);
+
+  gtk_widget_grab_focus(window);
   gtk_widget_show_all (window);
 }
 
-int
-main (int    argc,
-      char **argv)
+int main (int argc, char **argv)
 {
   GtkApplication *app;
   int status;
