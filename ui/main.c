a52ea6b78874969a4919a7f6777ef5a0fbf7ee5b
Yury <kotovyuriy27@gmail.com>
2022-05-09 00:07:18 +0300

    added signup widget

--- ui/main.c
+++ ui/main.c
@@ -6,7 +6,14 @@ GtkWidget *window;
 GtkWidget* nick_name_box;
 GtkWidget* password_box;
 GtkWidget* submit_button;
+GtkWidget* create_account_button;
+
 GtkWidget* login_box;
+
+GtkWidget* signup_box;
+GtkWidget* signup_nickname_box;
+GtkWidget* signup_password_box;
+GtkWidget* signup_submit_button;
 
 void submit(GtkWidget* widget, GdkEvent  *event, gpointer data)
 {
@@ -30,6 +37,39 @@ void submit(GtkWidget* widget, GdkEvent  *event, gpointer data)
   std::cout << "password " << password << std::endl;
 
   gtk_container_remove(GTK_CONTAINER(window), login_box);
+  gtk_container_add(GTK_CONTAINER(window), signup_box);
+  gtk_widget_show_all (window);
+}
+
+void signup_submit(GtkWidget* widget, GdkEvent  *event, gpointer data) {
+  const char* nick = gtk_entry_get_text(GTK_ENTRY(signup_nickname_box));
+  const char* password = gtk_entry_get_text(GTK_ENTRY(signup_password_box));
+
+  if (strlen(nick) == 0)
+  {
+    std::cout << "enter nick\n" << std::endl;
+    return;
+  }
+
+  // message must not be empty
+  if (strlen(password) == 0)
+  {
+      std::cout << "enter password" << std::endl;
+      return;
+  }
+
+  std::cout << "nick " << nick << std::endl;
+  std::cout << "password " << password << std::endl;
+
+  gtk_container_remove(GTK_CONTAINER(window), signup_box);
+  gtk_container_add(GTK_CONTAINER(window), login_box);
+  gtk_widget_show_all (window);
+}
+
+void onCreateAccountButtonClick(GtkWidget* widget, GdkEvent  *event, gpointer data) {
+  gtk_container_remove(GTK_CONTAINER(window), login_box);
+  gtk_container_add(GTK_CONTAINER(window), signup_box);
+  gtk_widget_show_all (window);
 }
 
 static void activate (GtkApplication *app, gpointer user_data)
@@ -38,6 +78,24 @@ static void activate (GtkApplication *app, gpointer user_data)
   gtk_window_set_title (GTK_WINDOW (window), "Chat");
   gtk_window_set_default_size (GTK_WINDOW (window), 600, 800);
 
+  // signup_box
+  signup_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+
+  signup_nickname_box = gtk_entry_new();
+  gtk_widget_set_size_request(signup_nickname_box, 200, -1);
+  gtk_entry_set_placeholder_text(GTK_ENTRY(signup_nickname_box), "nickname");
+  gtk_box_pack_start(GTK_BOX(signup_box), signup_nickname_box, FALSE, TRUE, 0);
+
+  signup_password_box = gtk_entry_new();
+  gtk_widget_set_size_request(signup_password_box, 200, -1);
+  gtk_entry_set_placeholder_text(GTK_ENTRY(signup_password_box), "password");
+  gtk_box_pack_start(GTK_BOX(signup_box), signup_password_box, FALSE, TRUE, 0);
+
+  signup_submit_button = gtk_button_new_with_label("signup");
+	gtk_box_pack_start(GTK_BOX(signup_box), signup_submit_button, FALSE, TRUE, 0);
+  g_signal_connect(signup_submit_button, "clicked", G_CALLBACK(signup_submit), NULL);
+
+  // login box
   login_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   // gtk_box_set_center_widget(GTK_CONTAINER(window), login_box);
 
@@ -54,6 +112,10 @@ static void activate (GtkApplication *app, gpointer user_data)
   submit_button = gtk_button_new_with_label("login");
 	gtk_box_pack_start(GTK_BOX(login_box), submit_button, FALSE, TRUE, 0);
   g_signal_connect(submit_button, "clicked", G_CALLBACK(submit), NULL);
+
+  create_account_button = gtk_button_new_with_label("create new account");
+	gtk_box_pack_start(GTK_BOX(login_box), create_account_button, FALSE, TRUE, 0);
+  g_signal_connect(create_account_button, "clicked", G_CALLBACK(onCreateAccountButtonClick), NULL);
 
   gtk_container_add(GTK_CONTAINER(window), login_box);
 
