e5f875d4271747e5db4dddbfa6da26254123d46c
Totenkaf <uscov.1998@mail.ru>
2022-05-25 15:07:18 +0300

    CI added

--- project/ui/src/chat.cpp
+++ project/ui/src/chat.cpp
@@ -67,15 +67,3 @@ void activate_chat(GtkApplication *app) {
     gtk_widget_show_all(window);
     return;
 }
-
-// int main(int argc, char **argv) {
-//     GtkApplication *app;
-//     int status;
-
-//     app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-//     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-//     status = g_application_run(G_APPLICATION(app), argc, argv);
-//     g_object_unref(app);
-
-//     return status;
-// }
--- project/ui/src/search.cpp
+++ project/ui/src/search.cpp
@@ -30,15 +30,3 @@ void activate_search(GtkApplication *app) {
     gtk_widget_show_all(window_);
     return;
 }
-
-// int main(int argc, char **argv) {
-//     GtkApplication *app;
-//     int status;
-
-//     app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-//     g_signal_connect(app, "activate", G_CALLBACK(activate_search), NULL);
-//     status = g_application_run(G_APPLICATION(app), argc, argv);
-//     g_object_unref(app);
-
-//     return status;
-// }
